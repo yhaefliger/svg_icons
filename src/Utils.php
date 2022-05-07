4381efa6f45ca054a2fbc0313f8135aa9e8e6e37
Yann Haefliger <yhaefliger@gmail.com>
2022-05-07 13:56:09 +0200

    merge

--- src/Utils.php
+++ src/Utils.php
@@ -9,7 +9,8 @@ use Drupal\Core\Template\Loader\FilesystemLoader;
 /**
  * Utility class for SVG icons.
  */
-class Utils {
+class Utils
+{
 
 
   /**
@@ -41,7 +42,8 @@ class Utils {
    * @param \Drupal\Core\Template\Loader\FilesystemLoader $loader
    *   Twig loader.
    */
-  public function __construct(FileSystem $filesystem, FilesystemLoader $loader) {
+  public function __construct(FileSystem $filesystem, FilesystemLoader $loader)
+  {
     $this->filesystem = $filesystem;
     $this->loader = $loader;
     $this->config = \Drupal::config('svg_icons.settings');
@@ -53,7 +55,8 @@ class Utils {
    * @return string
    *   SVG icon.
    */
-  public function defaultClass() {
+  public function defaultClass()
+  {
     return $this->config->get('default_class');
   }
 
@@ -66,13 +69,13 @@ class Utils {
    * @return string|false
    *   The full path or null if not found.
    */
-  public function getPath($path = NULL) {
+  public function getPath($path = NULL)
+  {
     $path = $path ? $path : $this->config->get('path');
 
     if (!empty($path)) {
       return $this->getRealPath($path);
-    }
-    else {
+    } else {
       throw new \Exception('No svg path configured.');
     }
   }
@@ -86,11 +89,11 @@ class Utils {
    * @return bool
    *   True if path is valid.
    */
-  public function isValidPath(string $path) {
+  public function isValidPath(string $path)
+  {
     if ($this->getRealPath($path)) {
       return TRUE;
-    }
-    else {
+    } else {
       return FALSE;
     }
   }
@@ -104,7 +107,8 @@ class Utils {
    * @return string|false
    *   The full path or false if not found.
    */
-  public function getRealPath(string $path) {
+  protected function getRealPath(string $path)
+  {
     // Twig loader replace @theme/@module with template dir.
     if (substr($path, 0, 1) == '@') {
       $folders = explode('/', $path);
@@ -112,13 +116,11 @@ class Utils {
       $base_path = str_replace('/templates', '', $this->loader->getPaths(str_replace('@', '', $base)));
       if (!empty($base_path)) {
         $path = current($base_path) . '/' . implode('/', $folders);
-      }
-      else {
+      } else {
         return FALSE;
       }
     }
 
     return $this->filesystem->realpath($path);
   }
-
 }
