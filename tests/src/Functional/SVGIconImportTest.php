f3d6325d29a5c91c17aaca59e3cb270d697930a8
Yann Haefliger <yhaefliger@gmail.com>
2022-06-09 11:43:15 +0200

    functional test submit form

--- tests/src/Functional/SVGIconImportTest.php
+++ tests/src/Functional/SVGIconImportTest.php
@@ -24,8 +24,20 @@ class SVGIconImportTest extends SVGIconFunctionalTestBase {
 
     $this->createMediaType();
     $this->drupalGet('/admin/config/media/svg-icons/import');
-    $this->assertSession()->responseNotContains('No media bundles can handle svg file format');
+    $assert_session = $this->assertSession();
+    $assert_session->responseNotContains('No media bundles can handle svg file format');
+    $assert_session->fieldExists('path');
+    $assert_session->fieldExists('media_bundle');
+    $this->submitForm([], "Import icons into media", "svg-icons-import-form");
 
+    $this->drupalGet('/admin/content/media');
+    $assert_session = $this->assertSession();
+    // Media type check.
+    $assert_session->responseContains('>svg_icon</td>');
+    // Media Status check.
+    $assert_session->responseContains('<td>Published</td>');
+    // SVG Icon check.
+    $assert_session->responseContains('>druplicon.svg</a>');
   }
 
 }
